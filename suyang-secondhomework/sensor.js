d07073c46798c271c047695ec9b8e24e934ff962
sy <1433910681@qq.com>
2018-04-17 16:52:32 +0800

    new obj（八个点，随机数控制高度）

--- suyang-secondhomework/sensor.js
+++ suyang-secondhomework/sensor.js
@@ -1,37 +1,39 @@
 const dgram = require('dgram');
-var obj = {"id":1,"url":"Cesium/Apps/SampleData/models/CesiumBalloon/CesiumBalloon.glb","height":1000.0};
+
 setInterval(broadcastNew, 5000);
 function broadcastNew() {
   //replace obj with your own sensor data
-  /*such as some mathematical curve*/
+  var lon=[-112.110693];
+  var lat=[36.0994841];
+  var radius=0.03;
+  var toRadians=Math.PI/180.0;
+  var h=[];
+  var j=0;
+  for(var i = 0; i <= 360; i += 45 )
+  {
+	  var radians=i*toRadians; //转换为弧度
+	  lon[j+1]=lon[0] + (radius * 1.5 * Math.cos(radians));
+	  lat[j+1]=lat[0] + (radius * Math.sin(radians));
+	  h[j]=Math.floor(Math.random()*11)* 500 + 1750;//使用 Math 对象的 floor() 方法(下舍入)和 random() 来返回一个介于 0 和 11 之间的随机数：
+      j++;
+  }
+  var obj = [
+	  {"longitude":lon[1], "latitude":lat[1], "height":h[0]},
+	  {"longitude":lon[2], "latitude":lat[2], "height":h[1]},
+	  {"longitude":lon[3], "latitude":lat[3], "height":h[2]},
+	  {"longitude":lon[4], "latitude":lat[4], "height":h[3]},
+	  {"longitude":lon[5], "latitude":lat[5], "height":h[4]},
+	  {"longitude":lon[6], "latitude":lat[6], "height":h[5]},
+	  {"longitude":lon[7], "latitude":lat[7], "height":h[6]},
+	  {"longitude":lon[8], "latitude":lat[8], "height":h[7]},
+  ];
+ 
+  
   var myJSON = JSON.stringify(obj);
   const message = myJSON;
   const client = dgram.createSocket('udp4');
   client.send(message, 21234, 'localhost', (err) => {
     client.close();
-  switch (obj.id)
-  {
-	case 1:
-	obj = {"id":2,"url":"Cesium/Apps/SampleData/models/CesiumGround/Cesium_Ground.glb","height":0};
-	break;
-	case 2:
-	obj = {"id":3,"url":"Cesium/Apps/SampleData/models/CesiumMilkTruck/CesiumMilkTruck-kmc.glb","height":0};
-	break;
-	case 3:
-	obj = {"id":4,"url":"Cesium/Apps/SampleData/models/CesiumMan/Cesium_Man.glb","height":0};
-	break;
-	case 4:
-	obj = {"id":5,"url":"Cesium/Apps/SampleData/models/CesiumAir/Cesium_Air.glb","height":5000.0};
-	break;
-	case 5:
-	obj = {"id":6,"url":"Cesium/Apps/SampleData/models/DracoCompressed/CesiumMilkTruck.gltf","height":0};
-	break;
-	case 6:
-	obj = {"id":1,"url":"Cesium/Apps/SampleData/models/CesiumBalloon/CesiumBalloon.glb","height":1000.0};
-	break;
-	default:
-	break;
-  }
  
   });
   console.log("Sent " + message + " to the wire...");
