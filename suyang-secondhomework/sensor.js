7a53c8dd9cd037601a208ecb2ae286224752ca1c
sy <1433910681@qq.com>
2018-04-18 21:46:57 +0800

    首尾重合

--- suyang-secondhomework/sensor.js
+++ suyang-secondhomework/sensor.js
@@ -11,10 +11,17 @@ function broadcastNew() {
   var j=0;
   for(var i = 0; i <= 360; i += 45 )
   {
+	  if(j==8)
+	  {
+		 lon[j+1]=lon[1];
+		 lat[j+1]=lat[1];
+		 h[j]=h[0];
+		 break;
+	  }
 	  var radians=i*toRadians; //转换为弧度
 	  lon[j+1]=lon[0] + (radius * 1.5 * Math.cos(radians));
 	  lat[j+1]=lat[0] + (radius * Math.sin(radians));
-	  h[j]=Math.floor(Math.random()*11)* 500 + 1750;//使用 Math 对象的 floor() 方法(下舍入)和 random() 来返回一个介于 0 和 11 之间的随机数：
+	  h[j]=Math.floor(Math.random()*5)* 500 + 1750;//使用 Math random() 来返回一个介于 0 和 1 之间的随机数：
       j++;
   }
   var obj = [
@@ -26,6 +33,7 @@ function broadcastNew() {
 	  {"longitude":lon[6], "latitude":lat[6], "height":h[5]},
 	  {"longitude":lon[7], "latitude":lat[7], "height":h[6]},
 	  {"longitude":lon[8], "latitude":lat[8], "height":h[7]},
+	  {"longitude":lon[9], "latitude":lat[9], "height":h[8]},
   ];
  
   
